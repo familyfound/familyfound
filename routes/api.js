cf896a90b48c84e7d9d0b470664b4ec2ba519a1a
Jared Forsyth <jared@jaredforsyth.com>
2013-06-24 20:22:17 -0600

    adding login check

--- routes/api.js
+++ routes/api.js
@@ -9,12 +9,25 @@ var token = 'USYS0BE68484DB1264D7DD3146E6EDCC1358_naci-045-033.d.usys.fsglobal.n
 
 function getPerson(req, res) {
   if (!req.params.id) return {error: 'no person id'};
-  fs.get('persons-with-relationships', function (err, data) {
+  fs.get('person-with-relationships-query',
+         {person: req.params.id},
+         req.session.oauth.access_token,
+         function (err, data) {
+    if (err) {
+      return res.send(401, {error: 'Not logged in'});
+    }
     debug('got', data);
-    res.send({success: true});
+    return res.send(data);
   });
 }
 
+function checkToken(req, res, next) {
+  if (!req.session.oauth || !req.session.oauth.access_token) {
+    return res.send(401, {error: 'Not logged in'});
+  }
+  next();
+}
+
 exports.addRoutes = function (app) {
-  app.get('/api/person/:id', getPerson);
+  app.get('/api/person/:id', checkToken, getPerson);
 };
